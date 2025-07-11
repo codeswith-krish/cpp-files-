772ec378e7a3ecc1400304a5aaf4cd2db4baf8e2
codeswith-krish <10krish.kr@gmail.com>
2025-07-11 23:35:42 +0530

    perimeter of rectangle

---
+++ perimeter.cpp
@@ -0,0 +1,14 @@
+// Calculate area and perimeter of a rectangle
+#include<iostream>
+using namespace std;
+int main()
+{
+  int length, bredth;
+  cout<<"input lenth of reactangle : ";
+  cin>>length;
+  cout<<"input breadth of rectangle : ";
+  cin>>bredth;
+  cout<<"perimeter of rectangle : "<<2*(length+bredth);
+
+  return 0;
+}
