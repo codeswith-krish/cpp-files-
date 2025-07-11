b388c6cf3eee71c48f5a5d0668c9134f1a6c84a3
codeswith-krish <10krish.kr@gmail.com>
2025-07-11 23:31:13 +0530

    sum of two number

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+// Take two numbers as input and print their sum.
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1,num2;
+  cout<<"input 1st number : ";
+  cin>>num1;
+  cout<<"input 2nd number : ";
+  cin>>num2;
+  cout<<"sum of both number :  "<<num1+num2;
+
+  return 0;
+}
