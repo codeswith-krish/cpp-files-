5e8d1da71a4d514d090fe6d82964812f0c9eb120
codeswith-krish <10krish.kr@gmail.com>
2025-07-11 23:40:18 +0530

    Area of circle

---
+++ area_circle.cpp
@@ -0,0 +1,12 @@
+// WAP in cpp to find area of circle
+#include<iostream>
+using namespace std;
+int main()
+{
+  int radius;
+  cout<<"input radius of circle : ";
+  cin>>radius;
+  cout<<"area of circle : "<<(3.14*radius*radius);
+
+  return 0;
+}
