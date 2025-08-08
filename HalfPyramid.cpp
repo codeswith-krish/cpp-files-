86d220c4fb080eee2b8d8d04454438fe0e56be9c
codeswith-krish <10krish.kr@gmail.com>
2025-08-08 23:01:22 +0530

    shape of a half pyramid

---
+++ HalfPyramid.cpp
@@ -0,0 +1,16 @@
+// Half Pyramid
+#include <iostream>
+using namespace std;
+int main()
+{
+  for (int i = 1; i < 6; i++)
+  {
+    for (int j = 1; j < i + 1; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
