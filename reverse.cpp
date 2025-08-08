7fab7f44644236259e777a23c0bf5cc21bd01497
codeswith-krish <10krish.kr@gmail.com>
2025-08-08 22:40:09 +0530

    find reverse of any number

---
+++ reverse.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class reverse
+{
+public:
+  int num, rem, sum = 0;
+  void method()
+  {
+    cout << "input a number : ";
+    cin >> num;
+    while (num != 0)
+    {
+      rem = num % 10;
+      sum = sum * 10 + rem;
+      num = num / 10;
+    }
+  }
+  void display()
+  {
+    cout<<"reverse of the number : "<<sum;
+  }
+};
+int main()
+{
+ reverse a1;
+ a1.method();
+ a1.display(); 
+}
