e6c83bce21f0e9448657d7ddbb1725fd0a7150a5
codeswith-krish <10krish.kr@gmail.com>
2025-08-08 22:17:13 +0530

    value of X^y
    find value in X^y term

---
+++ power.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+class power{
+  public:
+  double num,power;
+  void method()
+  {
+    cout<<"input the number :";
+    cin>>num;
+    cout<<"input the power the number ";
+    cin>>power;
+    cout<<"simplify = "<<pow(num,power);
+  }
+};
+int main()
+{
+  power a1;
+  a1.method();
+}
