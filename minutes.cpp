397e2adb01eecf0ab372c0581152c2e179c12499
codeswith-krish <10krish.kr@gmail.com>
2025-08-11 22:59:26 +0530

    minutes in hour conversion

--- minutes.cpp
+++ minutes.cpp
@@ -1,15 +1,24 @@
-// convert minutes into hours 
-#include<iostream>
+// convert minutes into hours
+#include <iostream>
 using namespace std;
+class minutes
+{
+public:
+  int min, minutes;
+  double hr;
+  void method()
+  {
+    cout << "input minutes to convert : ";
+    cin >> min;
+    hr = min / 60;      // formula to find hours
+    minutes = min % 60; // formula to find remaining minutes after hour converstion
+    cout << min << " minutes in hour is shown as :  " << endl << hr << " hr " << minutes << " min";
+  }
+};
 int main()
 {
-  int min,minutes;
-  double hr;
-  cout<<"input minutes to convert : ";
-  cin>>min;
-  hr=min/60; // formula to find hours
-  minutes=min%60; // formula to find remaining minutes after hour converstion
-  cout<<min<<" minutes in hour = "<<hr<<" hr "<<minutes<<" min";
+  minutes a1;
+  a1.method();
 
   return 0;
-} 
+}
