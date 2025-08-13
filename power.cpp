acb1667595b1bd5f7271f1fd6e82a110bbae6980
codeswith-krish <10krish.kr@gmail.com>
2025-08-13 21:22:05 +0530

    Update power.cpp

--- power.cpp
+++ power.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
-#include<math.h>
+// find the value of a exponential term x^y
+#include <iostream>
+#include <math.h>
 using namespace std;
-class power{
-  public:
-  double num,power;
+class power
+{
+public:
+  double num, power;
   void method()
   {
-    cout<<"input the number :";
-    cin>>num;
-    cout<<"input the power the number ";
-    cin>>power;
-    cout<<"simplify = "<<pow(num,power);
+    cout << "input the number :";
+    cin >> num;
+    cout << "input the power the number ";
+    cin >> power;
+    cout << "simplify = " << pow(num, power);
   }
 };
 int main()
